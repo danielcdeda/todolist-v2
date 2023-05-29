2c30ea53f729684caddc345c5632375ecaf9b1b2
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-05-29 16:27:41 -0300

    added nodemon

--- app.js
+++ app.js
@@ -15,23 +15,27 @@ mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true}
  // Schema below // 
 
 const itemsSchema = new mongoose.Schema ({
+  _id : Number, 
   name: String,
-
 });
+
  // Model Below //
 
 const Item = new mongoose.model ("Item", itemsSchema)
 
 // Documents below //
 const item1 = new Item ({
+  _id : 1,
   name: "Welcome to your todolist"
 })
 
 const item2 = new Item ({
+  _id : 2,
   name: "Hit the + button to add a new item."
 })
 
 const item3 = new Item ({
+  _id : 3,
   name: "<-- Hit this to delete an item"
 })
 
@@ -46,9 +50,15 @@ Item.insertMany([defaultItems])
 });
 
 
+
 app.get("/", function(req, res) {
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}).then(function(foundItems){
+    res.render("list", { listTitle: "Today", newListItems: foundItems });
+  })
+  .catch(function(err){
+    console.log(err);
+  });
 
 });
 
@@ -73,6 +83,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(2048, function() {
+  console.log("Server started on port 2048");
 });
