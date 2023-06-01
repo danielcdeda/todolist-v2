9273111a1978380eca2335fd1065b14bcf03199b
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-06-01 11:11:30 -0300

    .

--- app.js
+++ app.js
@@ -68,19 +68,23 @@ app.get("/", function(req, res) {
 
 });
 
-
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-});
+  app.post("/", function(req, res){
+ 
+    const itemName = req.body.newItem;
+   
+    const item = new Item({
+      name: itemName
+    });
+   
+    item.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+   
+  });
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
